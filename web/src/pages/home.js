bfaa53b58143a44b73c431846a292a5b6be193a0
Kendsdavs <kendra.wieck@gmail.com>
2016-11-18 11:55:32 -0500

    step 2 xhr

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -5,12 +5,13 @@ const Home = React.createClass({
   render() {
     return (
       <div>
-        <h1>Welcome home</h1>
+        <h1>Welcome Home</h1>
         <h3>Menu</h3>
         <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
+          <li><Link to="/persons">Persons</Link></li>
+          <li><Link to="/efforts">Efforts</Link></li>
+          <li><Link to="/locations">Locations</Link></li>
+          <li><Link to="/about">About</Link></li>
         </ul>
 
       </div>

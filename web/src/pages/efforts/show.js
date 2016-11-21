f233682046929fcba1138e6d3ed20d6d5259d1d3
Kendsdavs <kendra.wieck@gmail.com>
2016-11-21 11:23:19 -0500

    effort step 5 delete-effort

--- web/src/pages/efforts/show.js
+++ web/src/pages/efforts/show.js
@@ -1,11 +1,12 @@
 const React = require('react')
 const xhr = require('xhr')
-const {Link} = require('react-router')
+const {Link, Redirect} = require('react-router')
 
 const Effort = React.createClass({
   getInitialState() {
     return {
-      effort: {}
+      effort: {},
+      removed: false
     }
   },
   componentDidMount() {
@@ -17,15 +18,30 @@ const Effort = React.createClass({
       this.setState({effort})
     })
   },
+  handleRemove(e) {
+    e.preventDefault()
+    if (confirm('Are you sure?') ) {
+      xhr.del('http://127.0.0.1:4000/efforts/' + this.state.effort.id, {
+        json: this.state.effort
+      }, (err, res, body) => {
+        if (err) return console.log(err.message)
+        this.setState({removed: true})
+      })
+    }
+  },
   render() {
     return (
       <div>
+        {this.state.removed ? <Redirect to="/efforts" /> : null}
         <h3>{this.state.effort.name}</h3>
         <Link to="/efforts">Return</Link>
         |
         <Link to={`/efforts/${this.state.effort.id}/edit`}>
           Edit Effort
         </Link>
+        |
+        |
+        <a href="#" onClick={this.handleRemove}>Remove Effort</a>
 
       </div>
     )
